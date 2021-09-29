76796affb512927002925c0c6d68e519ac5549b0
srushtidesaii <90732642+srushtidesaii@users.noreply.github.com>
2021-09-29 13:33:23 -0400

    Update COVIDtableauSQLQuery4.sql

--- COVIDtableauSQLQuery4.sql
+++ COVIDtableauSQLQuery4.sql
@@ -1,3 +1,7 @@
+--COVID DASHBOARD
+--https://public.tableau.com/app/profile/srushti.desai/viz/COVIDDashboard_16329364830520/Dashboard1?publish=yes
+
+
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
 --Where location like '%states%'
